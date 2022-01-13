3c5cf84c6a1a0a67f93d70bd5c13bc881f5825f1
HAYEL <i.hayel.dev@gmail.com>
2022-01-14 00:15:33 +0300

    Runner updated

--- com/teachmeskills/task_3/Runner.java
+++ com/teachmeskills/task_3/Runner.java
@@ -2,6 +2,17 @@ package com.teachmeskills.task_3;
 
 import com.teachmeskills.task_3.model.test.FunctionalInterfaceExampleTest;
 
+/**
+ * Create a generic functional interface.
+ * Write a class with one method (think and reason the decision about whether it will be a static method or not).
+ * In this method, implement the logic:
+ * - if the number 1 is entered into the console, then: use the interface with a string type
+ * and pass the logic of string reversal (outputting the string in reverse order) to the interface method.
+ *
+ * - if the number 2 is entered into the console, then: use an interface with an integer type and
+ * pass the logic of finding the factorial of a number to the interface method.
+ */
+
 public class Runner {
     public static void main(String[] args) {
         FunctionalInterfaceExampleTest.test();
